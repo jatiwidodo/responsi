ea9be38947ab9203d0f97ad9c954da1184dc90de
unknown <priambodo.widodo@students.amikom.ac.id>
2019-05-01 20:10:35 +0700

    responsi

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2279
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //objek Karyawan baru
+            Karyawan karyawan1 = new Karyawan("18.11.2279", "jaty", 4000000);
+            Karyawan karyawan2 = new Karyawan("22.79.1118", "paijo", 3000000);
+
+            //Nampilkan Gaji
+            Console.WriteLine("No NIK/Nama            Gaji Bulanan");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("1. {0} {1}        {2}", karyawan1.Nik, karyawan1.Nama, karyawan1.GajiBulanan);
+            Console.WriteLine("2. {0} {1}         {2}", karyawan2.Nik, karyawan2.Nama, karyawan2.GajiBulanan);
+
+            Console.WriteLine();
+
+            //naik Gaji
+            karyawan1.NaikGaji();
+            karyawan2.NaikGaji();
+
+            //Nampilkan Gaji
+            Console.WriteLine("No NIK/Nama            Gaji Bulanan");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("1. {0} {1}        {2}", karyawan1.Nik, karyawan1.Nama, karyawan1.GajiBulanan);
+            Console.WriteLine("2. {0} {1}         {2}", karyawan2.Nik, karyawan2.Nama, karyawan2.GajiBulanan);
+
+
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2279
+{
+    class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public Karyawan(string nik, string nama, int gaji)
+        {
+            Nik = nik;
+            Nama = nama;
+            GajiBulanan = gaji;
+        }
+
+        //method
+        public void NaikGaji()
+        {
+            GajiBulanan = GajiBulanan + GajiBulanan / 10;
+        }
+
+    }
+}
